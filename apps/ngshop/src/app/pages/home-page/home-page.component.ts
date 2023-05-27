2c04a5860b3292d47c026569c83825dc1c9c4972
guruece24 <guruece24@gmail.com>
2023-05-27 21:22:18 +0530

    primengcomponents

--- apps/ngshop/src/app/pages/home-page/home-page.component.ts
+++ apps/ngshop/src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'bluebits-home-page',
+    selector: 'ngshop-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss']
 })
