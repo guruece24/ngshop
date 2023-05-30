235e8c1588661e48f0f5975870071601c16ced04
guruece24 <guruece24@gmail.com>
2023-05-30 15:07:54 +0530

    adminside

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            }
+        ]
+    },
+    {
+        path: 'products',
+        component: SidebarComponent,
+        children: [
+          {
+              path: 'dashboard',
+              component: DashboardComponent
+          }
+      ]
+    }
+];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+    declarations: [
+        AppComponent,
+        NxWelcomeComponent,
+        DashboardComponent,
+        ShellComponent,
+        SidebarComponent
+    ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    exports: [ShellComponent, SidebarComponent]
 })
 export class AppModule {}
