a947b29748d636a00b05ddc4384ca5d013925c2a
guruece24 <guruece24@gmail.com>
2023-06-02 19:04:53 +0530

    categoriesadmin

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+const UX_MODULE = [CardModule, ToolbarModule, ButtonModule, TableModule];
 
 const routes: Routes = [
     {
@@ -20,14 +27,8 @@ const routes: Routes = [
         ]
     },
     {
-        path: 'products',
-        component: SidebarComponent,
-        children: [
-          {
-              path: 'dashboard',
-              component: DashboardComponent
-          }
-      ]
+        path: 'categories',
+        component: CategoriesListComponent
     }
 ];
 
@@ -37,14 +38,16 @@ const routes: Routes = [
         NxWelcomeComponent,
         DashboardComponent,
         ShellComponent,
-        SidebarComponent
+        SidebarComponent,
+        CategoriesListComponent
     ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })
+        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+        ...UX_MODULE
     ],
     providers: [],
     bootstrap: [AppComponent],
-    exports: [ShellComponent, SidebarComponent]
+    exports: [ShellComponent, SidebarComponent, CategoriesListComponent]
 })
 export class AppModule {}
---
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit  } from '@angular/core';
+
+@Component({
+    selector: 'admin-categories-list',
+    templateUrl: './categories-list.component.html',
+    styles: []
+})
+export class CategoriesListComponent implements OnInit {
+  
+   categories = [
+            {
+                id: 1,
+                name: 'category-1',
+                icon: 'icon-1',
+            },
+            {
+                id: 2,
+                name: 'category-2',
+                icon: 'icon-2',
+            },
+            {
+                id: 3,
+                name: 'category-3',
+                icon: 'icon-3',
+            }
+        ];
+
+     constructor() { 
+     // console.log(this.categories); 
+    }
+
+    ngOnInit(): void { 
+      console.log(this.categories);
+    } 
+    
+}
