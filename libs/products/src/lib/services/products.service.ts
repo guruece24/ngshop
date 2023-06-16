6d5b26fdfd1e18cfb192ab4bb806620a4c8064f8
guruece24 <guruece24@gmail.com>
2023-06-16 21:20:16 +0530

    prodsgetall

--- libs/products/src/lib/services/products.service.ts
+++ libs/products/src/lib/services/products.service.ts
@@ -13,7 +13,7 @@ export class ProductsService {
     constructor(private http: HttpClient) {}
 
     getProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.apiURLProducts);
+        return this.http.get<Product[]>(`${this.apiURLProducts}/all`);
     }
 
     //  getCategoryById(categoryId : string): Observable<Product> {
