636b68e8049363c3cb9f4f4616375f1fa24bf19a
guruece24 <guruece24@gmail.com>
2023-06-26 17:04:57 +0530

    header-animate

---
+++ libs/products/src/lib/components/categories-banner/categories-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-categories-banner',
+  templateUrl: './categories-banner.component.html',
+  styles: [
+  ]
+})
+export class CategoriesBannerComponent {
+
+}
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
+import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    ProductsSearchComponent
+    ProductsSearchComponent,
+    CategoriesBannerComponent
   ],
   exports: [
-    ProductsSearchComponent
+    ProductsSearchComponent,
+    CategoriesBannerComponent
   ]
 })
 
