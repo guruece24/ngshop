8c5218edad7516f08e71522e17fa456d2baafa37
guruece24 <guruece24@gmail.com>
2023-06-27 17:28:27 +0530

    catsbanner

--- libs/products/src/lib/components/categories-banner/categories-banner.component.ts
+++ libs/products/src/lib/components/categories-banner/categories-banner.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
+import { Category } from '../../models/category';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'products-categories-banner',
-  templateUrl: './categories-banner.component.html',
-  styles: [
-  ]
+    selector: 'products-categories-banner',
+    templateUrl: './categories-banner.component.html',
+    styles: []
 })
-export class CategoriesBannerComponent {
 
+export class CategoriesBannerComponent implements OnInit, OnDestroy {
+    categories: Category[] = [];
+    endSubs$: Subject<any> = new Subject();
+
+    constructor(private categoriesService: CategoriesService) {}
+
+    ngOnInit(): void {
+        this.categoriesService
+            .getCategories()
+            .pipe(takeUntil(this.endSubs$))
+            .subscribe((cats) => {
+                this.categories = cats;
+                //console.log(this.categories);
+            });
+    }
+
+    ngOnDestroy() {
+      this.endSubs$.next(0);
+      this.endSubs$.complete();
+    }
 }
