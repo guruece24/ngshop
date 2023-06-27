ec58fdd202cf8b244da9ff848c3cee5219bd3f3e
guruece24 <guruece24@gmail.com>
2023-06-27 17:43:31 +0530

    catsbannerrouterlink

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
+import { RouterModule } from "@angular/router";
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [
     ProductsSearchComponent,
     CategoriesBannerComponent
