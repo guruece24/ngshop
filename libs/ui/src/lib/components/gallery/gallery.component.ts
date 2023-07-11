983388eeb514d743ed04790881b672928a8c0814
guruece24 <guruece24@gmail.com>
2023-07-11 15:10:32 +0530

    prodselectedimageURL

--- libs/ui/src/lib/components/gallery/gallery.component.ts
+++ libs/ui/src/lib/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 // import { Product } from '@bluebits/products';
 
 @Component({
@@ -6,28 +6,32 @@ import { Component, Input, OnInit } from '@angular/core';
     templateUrl: './gallery.component.html',
     styles: []
 })
+export class GalleryComponent {
+    @Input() images = [];
+    @Input() selectedImageURL: any;
 
-export class GalleryComponent implements OnInit {
-    @Input() pImages = [];
-    @Input() selectedImage:any;
-   // @Input() product: Product = new Product();
+    // @Input() product: Product = new Product();
     //selectedImage = '';
 
-   constructor() {
-    this.pImages = [];
-    console.log(this.pImages);
-}
+    //    constructor() {
+    //     this.images = [];
+    //     console.log(this.images);
+    // }
 
-    ngOnInit(): void {
-        console.log(this.pImages);
-        //if (this.hasImages) {
-            // this.selectedImage='http://localhost:3000/public/uploads/5f15da135420d44456ed8e9e-1604665545872.jpeg';
-            //this.selectedImage = this.images[1];
-            //console.log(this.images);
-        //}
-    }
+    //     ngOnInit(): void {
+    //         console.log(this.images);
+    //         //if (this.hasImages) {
+    //             // this.selectedImage='http://localhost:3000/public/uploads/5f15da135420d44456ed8e9e-1604665545872.jpeg';
+    //             //this.selectedImage = this.images[1];
+    //             //console.log(this.images);
+    //         //}
+    //     }
 
-    get hasImages() {
-        return this.pImages?.length > 0;
+        get hasImages() {
+            return this.images?.length > 0;
+        }
+
+    changeSelectedImage(imageUrl: string) {
+        this.selectedImageURL = imageUrl;
     }
 }
