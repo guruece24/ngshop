6bacf7bc2058c9008dddda3b249d47aaa272e286
guruece24 <guruece24@gmail.com>
2023-07-15 16:47:47 +0530

    autherror

--- libs/users/src/lib/pages/login/login.component.ts
+++ libs/users/src/lib/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'users-login',
@@ -9,13 +10,18 @@ import { AuthService } from '../../services/auth.service';
     styles: []
 })
 
-
 export class LoginComponent implements OnInit {
     loginFormGroup: FormGroup;
     isSubmitted: boolean = false;
     value!: string;
+    authError = false;
+    authMessage = 'Email or Password are wrong';
 
-    constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private auth: AuthService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {
         this._initLoginForm();
@@ -27,13 +33,23 @@ export class LoginComponent implements OnInit {
             password: ['', Validators.required]
         });
     }
+
     onSubmit() {
         this.isSubmitted = true;
 
         if (this.loginFormGroup.invalid) return;
 
-        this.auth.login(this.loginForm.email.value, this.loginForm.password.value).subscribe((user) =>{
-            console.log(user);
+        this.auth.login(this.loginForm.email.value, this.loginForm.password.value).subscribe({
+            next: (user) => {
+                this.authError = false;
+            },
+            error: (error: HttpErrorResponse) => {
+                this.authError = true;
+                if (error.status != 400) {
+                    this.authMessage = 'Error in the server! Please try again later!';
+                }
+            },
+            complete: () => setTimeout(() => this.router.navigate(['/products']), 2000)
         });
     }
 
