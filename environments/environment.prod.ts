ad786332d70d9c3103004d181082c2ecfa173cfe
guruece24 <guruece24@gmail.com>
2023-09-09 19:01:52 +0530

    deploy99

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://eshop-backend-angular-f0629d9296f1.herokuapp.com/api/v1'
+    apiUrl: 'https://eshop-backend-angular-f0629d9296f1.herokuapp.com/api/v1/'
 };
