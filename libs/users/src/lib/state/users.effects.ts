a888413252bd3bd32f14475d44fb70d776bf7492
guruece24 <guruece24@gmail.com>
2023-09-27 18:29:56 +0530

    usersfacadeinteg

--- libs/users/src/lib/state/users.effects.ts
+++ libs/users/src/lib/state/users.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { switchMap, catchError, of } from 'rxjs';
+import { switchMap, catchError, of, map, concatMap } from 'rxjs';
 import * as UsersActions from './users.actions';
 import * as UsersFeature from './users.reducer';
 import { UsersService } from '../services/users.service';
